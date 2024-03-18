1a4d2f83af0390bd68e7d6bab831cf9ca0cdfc0d
20100308@mail.wit.ie <20100308@mail.wit.ie>
2024-03-18 23:08:18 +0000

    first test passed for db connection

--- backend/spring-boot-ecommerce/src/main/java/com/first/springbootecommerce/SpringBootEcommerceApplication.java
+++ backend/spring-boot-ecommerce/src/main/java/com/first/springbootecommerce/SpringBootEcommerceApplication.java
@@ -2,7 +2,7 @@ package com.first.springbootecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 
 @SpringBootApplication
 public class SpringBootEcommerceApplication {
--- backend/spring-boot-ecommerce/src/test/java/com/first/springbootecommerce/DBConnectionTest.java
+++ backend/spring-boot-ecommerce/src/test/java/com/first/springbootecommerce/DBConnectionTest.java
@@ -4,14 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
+
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 @SpringBootTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
