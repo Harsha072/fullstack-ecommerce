cde5df3f66de3748a452a847f5b98481f87d5656
20100308@mail.wit.ie <20100308@mail.wit.ie>
2024-07-03 16:47:30 +0100

    Integration test- DB connection completed

--- backend/spring-boot-ecommerce/src/test/java/com/first/springbootecommerce/DBConnectionTest.java
+++ backend/spring-boot-ecommerce/src/test/java/com/first/springbootecommerce/DBConnectionTest.java
@@ -1,10 +1,13 @@
 package com.first.springbootecommerce;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.mysql.cj.jdbc.DatabaseMetaData;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -12,7 +15,7 @@ import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-
+@RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class DBConnectionTest {
@@ -21,9 +24,15 @@ public class DBConnectionTest {
 
     @Test
     public void testDatabaseConnection() {
+        System.out.println("connected to: " + dataSource.getClass().getName());
         try (Connection connection = dataSource.getConnection()) {
-            assertNotNull(connection, "Database connection successfuli");
-        } catch ( SQLException e) {
+            assertNotNull(connection, "Database connection successful");
+            java.sql.DatabaseMetaData metaData = connection.getMetaData();
+            String dbName = metaData.getDatabaseProductName();
+
+            System.out.println("Connected to database: " + dbName);
+
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
---
+++ backend/spring-boot-ecommerce/src/test/java/com/first/springbootecommerce/service/CheckoutServiceIntegrationTest.java
@@ -0,0 +1,49 @@
+package com.first.springbootecommerce.service;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class CheckoutServiceIntegrationTest {
+
+
+
+    @Autowired
+    private DataSource dataSource;
+
+    @BeforeAll
+    @Test
+    public void testDBConnection() {
+        assertNotNull(dataSource, "DataSource should not be null");
+
+        try (Connection connection = dataSource.getConnection()) {
+            assertNotNull(connection, "Database connection successful");
+            System.out.println("Connected to: " + dataSource.getClass().getName());
+            java.sql.DatabaseMetaData metaData = connection.getMetaData();
+            String dbName = metaData.getDatabaseProductName();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    
+}
